8876084f855add7914ac99137bbb3a364be00791
open-novel <32639927+open-novel@users.noreply.github.com>
2018-09-05 20:17:22 +0900

    u

--- プログラム/システム.js
+++ プログラム/システム.js
@@ -40,7 +40,7 @@ async function play ( ctx, mode, installEvent ) {
 
 	let sound = 'off'
 	if ( mode != 'install' ) {
-	let text = 'openノベルプレイヤー v1.0β_135   18/09/05' +
+	let text = 'openノベルプレイヤー v1.0β_136   18/09/05' +
 		( $.TEST.mode ? `  *${ $.TEST.mode } test mode*` : '' )
 
 		WHILE: while ( true ) {
